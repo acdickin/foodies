95b306489cf60c5e64e962e53a008e399472243b
Andrew Cosmic <acosmic@liveperson.com>
2024-04-30 11:36:09 -0700

    basic project start

---
+++ app/community/page.js
@@ -0,0 +1,10 @@
+export default function CommunityPage() {
+  return (
+    <main>
+      <h1 style={{ color: 'white', textAlign: 'center' }}>
+        Community Page
+      </h1>
+    </main>
+  );
+}
+ 
